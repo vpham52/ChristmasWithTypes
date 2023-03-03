d31d9457ebd8234828f45a3c1e046dca4a5720ed
Victoria Pham <victoriapham52@gmail.com>
2023-03-03 12:30:36 -0800

    added DONE to tasks

--- ChristmasWithTypes/Christmas.cs
+++ ChristmasWithTypes/Christmas.cs
@@ -5,9 +5,9 @@ namespace ChristmasWithTypes
     {
         public string[] Presents;
         public string Santa { get; set; }
-        public int? TreeHeight { get; set; } //TODO Make the Height property nullable
+        public int? TreeHeight { get; set; } //TODO Make the Height property nullable -- DONE
 
-        //TODO Make the property, "Day", type enum
+        //TODO Make the property, "Day", type enum -- DONE
         public enum Day { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
 
     }
--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -10,13 +10,13 @@ namespace ChristmasWithTypes
 
             var xmasDay = Christmas.Day.Thursday;
 
-            //TODO set Santa's name to Kris Kringle
+            //TODO set Santa's name to Kris Kringle -- DONE
             xmas.Santa = "Kris Kringle";
 
-            //TODO Insert 3 presents you would like for xmas.  They must be strings
+            //TODO Insert 3 presents you would like for xmas.  They must be strings --DONE
             xmas.Presents =  new string[3]{ "AirPods", "Cash", "Socks" }; 
 
-            //TODO Set the TreeHeight to 10
+            //TODO Set the TreeHeight to 10 -- DONE
             xmas.TreeHeight = 10;
 
             Console.WriteLine($"This year christmas falls on {xmasDay} \n");
